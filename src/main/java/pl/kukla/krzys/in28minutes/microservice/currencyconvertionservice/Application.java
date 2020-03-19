f3e13e0c37a98a30b50571de4c638caee4510677
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-20 00:33:44 +0100

    88 register service to Eureka naming server

--- src/main/java/pl/kukla/krzys/in28minutes/microservice/currencyconvertionservice/Application.java
+++ src/main/java/pl/kukla/krzys/in28minutes/microservice/currencyconvertionservice/Application.java
@@ -2,8 +2,12 @@ package pl.kukla.krzys.in28minutes.microservice.currencyconvertionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("pl.kukla.krzys.in28minutes.microservice.currencyconvertionservice")
+@EnableDiscoveryClient // it enables service as Eureka client
 public class Application {
 
 	public static void main(String[] args) {
