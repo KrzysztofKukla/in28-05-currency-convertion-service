e54d3e3f15f79a3f1b211da0766e4913502a9f03
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-19 15:03:00 +0100

    83 Ribbon client side load balancing

--- src/main/java/pl/kukla/krzys/in28minutes/microservice/currencyconvertionservice/service/client/CurrencyExchangeServiceProxy.java
+++ src/main/java/pl/kukla/krzys/in28minutes/microservice/currencyconvertionservice/service/client/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package pl.kukla.krzys.in28minutes.microservice.currencyconvertionservice.service.client;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,9 @@ import pl.kukla.krzys.in28minutes.microservice.currencyconvertionservice.web.mod
  */
 //it allows to talk to external microservice
 //'name' and 'url' of the service which we are going to call ( take from spring.application.name from .properties )
-@FeignClient(name = "04-currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "04-currency-exchange-service")
+//Ribbon allows to load balancing between all instances of microservice
+@RibbonClient(name = "04-currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/v1/currency-exchange/from/{from}/to/{to}")
